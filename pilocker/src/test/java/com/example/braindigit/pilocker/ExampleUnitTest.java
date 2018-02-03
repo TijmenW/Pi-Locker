b24f6b5c4ca1b1126bc5413fd22c5acb2d4a85f8
Tijmen Wildervanck <Tijmen.Wildervanck@gmail.com>
2018-02-03 19:34:48 +0100

    tried something else

--- pilocker/src/test/java/com/example/braindigit/pilocker/ExampleUnitTest.java
+++ pilocker/src/test/java/com/example/braindigit/pilocker/ExampleUnitTest.java
@@ -1,12 +1,14 @@
 package com.example.braindigit.pilocker;
 
 import static junit.framework.Assert.assertEquals;
+import junit.framework.Test;
+
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
-    @Test
+    //@Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
